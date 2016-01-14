8fb75b19efae27a7e7cfe3f69f61b24f4fd14f54
Chaz <cmtlum73@gmail.com>
2016-01-13 17:22:03 -1000

    cleaned up and added comments

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 var express = require( 'express' );
 var app = express();
-var bodyParser = require( 'body-parser');
+var bodyParser = require( 'body-parser' );
 
 app.use( express.static( 'public' ) );
 app.use( bodyParser.urlencoded( { extended : true } ) );
 
 var buzzWords = [];
-  console.log( buzzWords );
 
+//========== Retrieves the current list of buzzWords that have been posted to the server
 app.get( '/buzzwords', function (req, res ) {
   res.send( { buzzWords : buzzWords } );
 });
 
+//========== Adds new buzzWords to the array
 app.post( '/buzzword', function ( req, res ) {
   buzzWords.push( {
     buzzWord : req.body.buzzWord,
     points : req.body.points,
     heard : false
-  });
+  } );
   res.send( { 'success' : true } );
   console.log( buzzWords );
 });
+
 
 var server = app.listen( 7777, function () {
   var host = 'localhost';
